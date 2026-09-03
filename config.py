dc8a6cf9a54f5176e69ec6e465ebf015265f907e
Sarthak Agarwal <sarth@Sarthaks-Macbook.local>
2026-09-04 01:53:19 +0530

    Cleaning up the directory, making a seperate evaluate and config file tmake it more organised

---
+++ config.py
@@ -0,0 +1,13 @@
+import chess
+
+class material_value:
+    PIECE_VALUE = {
+    chess.PAWN: 100.0,
+    chess.KNIGHT: 320.0,
+    chess.BISHOP: 330.0,
+    chess.ROOK: 500.0,
+    chess.QUEEN: 900.0,
+    chess.KING: 1000.0 # Low val for move ordering
+    }
+    MOBILITY_WEIGHT = 4.0
+    
